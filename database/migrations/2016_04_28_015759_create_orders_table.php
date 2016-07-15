9c1e224655f7634cdbaef83c254d3816396f0b8b
felipebfernandes92 <fernandesfelipeb@gmail.com>
2016-07-14 21:04:00 -0300

    Correção migration orders.

--- database/migrations/2016_04_28_015759_create_orders_table.php
+++ database/migrations/2016_04_28_015759_create_orders_table.php
@@ -17,7 +17,7 @@ class CreateOrdersTable extends Migration
             $table->integer('client_id')->unsigned();
             $table->foreign('client_id')->references('id')->on('users');
             $table->integer('user_deliveryman_id')->unsigned()->nullable();
-            $table->foreign('user_deliveryman_id')->references('id')->on('user');
+            $table->foreign('user_deliveryman_id')->references('id')->on('users');
             $table->decimal('total');
             $table->smallInteger('status')->default(0);
 
