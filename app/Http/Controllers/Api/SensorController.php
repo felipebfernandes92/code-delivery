3ab21ff1f2d29c03ef5c0416c214ff621125a0fe
Felipe <fernandesfelipeb@gmail.com>
2017-05-08 21:27:16 -0300

    Adicionado push notification quando acaba a água.

--- app/Http/Controllers/Api/SensorController.php
+++ app/Http/Controllers/Api/SensorController.php
@@ -5,17 +5,21 @@ namespace CodeDelivery\Http\Controllers\Api;
 use CodeDelivery\Http\Controllers\Controller;
 use CodeDelivery\Repositories\SensorRepository;
 use Illuminate\Http\Request;
+use Dmitrovskiy\IonicPush\PushProcessor;
 
 class SensorController extends Controller
 {
 
     private $repository;
+    private $pushProcessor;
 
     public function __construct(
-        SensorRepository $repository
+        SensorRepository $repository,
+        PushProcessor $pushProcessor
     )
     {
         $this->repository = $repository;
+        $this->pushProcessor = $pushProcessor;
     }
 
     public function update(Request $request)
@@ -26,6 +30,12 @@ class SensorController extends Controller
         if($sensor) {
             $sensor['status'] = $dados['status'];
             $sensor->save();
+
+            if ($sensor->client->user->device_token && $dados['status'] == 2) {
+                $this->pushProcessor->notify([$sensor->client->user->device_token], [
+                    'message' => "Sua água está acabando!"
+                ]);
+            }
         }
 
         return $sensor;
